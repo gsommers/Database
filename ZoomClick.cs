74b2b42f5f3757b41f42443e83d3c20163b9dff3
gsommers <gsommers@princeton.edu>
2017-06-24 22:21:45 -0400

    Testing this out...

---
+++ ZoomClick.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ZoomClick : MonoBehaviour {
+
+    private int count;
+    public GameObject nextImage;
+    public RectTransform simple;
+    public float scale;
+    public Vector3 position;
+    private void Start()
+    {
+        count = 0;
+        nextImage.SetActive(false);
+    }
+
+    void OnMouseUpAsButton()
+    {
+        count++;
+        Debug.Log("You clicked Italy! Count: " + count);
+        nextImage.SetActive(true);
+        ScrollRect scroll = GetComponentInParent<ScrollRect>();
+        scroll.content = nextImage.GetComponent<RectTransform>();
+        simple.localScale = new Vector2(scale, scale);
+        simple.localPosition = position;
+    }
+
+}
